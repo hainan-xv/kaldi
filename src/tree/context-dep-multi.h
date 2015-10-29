6fad6086c63665ac96a0224b1d99566e7af1b972
Hainan Xu <hainan.xv@gmail.com>
2015-10-29 15:10:13 -0400

    move ContextDependencyMulti to seperate files

---
+++ src/tree/context-dep-multi.h
@@ -0,0 +1,122 @@
+// tree/context-dep.h
+
+// Copyright 2009-2011  Microsoft Corporation
+//           2015       Hainan Xu
+
+// See ../../COPYING for clarification regarding multiple authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef KALDI_TREE_CONTEXT_DEP_MULTI_H_
+#define KALDI_TREE_CONTEXT_DEP_MULTI_H_
+
+#include "itf/context-dep-itf.h"
+#include "tree/event-map.h"
+#include "matrix/matrix-lib.h"
+#include "tree/cluster-utils.h"
+#include "tree/context-dep.h"
+
+namespace kaldi {
+
+class ContextDependencyMulti: public ContextDependencyInterface {
+ public:
+  virtual int32 ContextWidth() const { return N_; }
+  virtual int32 CentralPosition() const { return P_; }
+
+  /// returns success or failure; outputs pdf to pdf_id
+  virtual bool Compute(const std::vector<int32> &phoneseq,
+                       int32 pdf_class, int32 *pdf_id) const;
+
+  virtual int32 NumPdfs() const {
+    // this routine could be simplified to return to_pdf_->MaxResult()+1.  we're a
+    // bit more paranoid than that.
+    if (!to_pdf_) return 0;
+    EventAnswerType max_result = to_pdf_->MaxResult();
+    if (max_result < 0 ) return 0;
+    else return (int32) max_result+1;
+  }
+  virtual ContextDependencyInterface *Copy() const {
+    vector<EventMap*> trees;
+    for (int i = 0; i < single_trees_.size(); i++) {
+      trees.push_back(single_trees_[i]->Copy());
+    }
+
+    return new ContextDependencyMulti(N_, P_, trees);
+  }
+
+  /// Read context-dependency object from disk; throws on error
+  void Read (std::istream &is, bool binary);
+
+  // Constructor with no arguments; will normally be called
+  // prior to Read()
+  ContextDependencyMulti(): N_(0), P_(0), to_pdf_(NULL) { }
+
+  // Constructor takes ownership of pointers.
+  ContextDependencyMulti(int32 N, int32 P,
+                         vector<EventMap*> single_trees):
+      N_(N), P_(P), single_trees_(single_trees) {
+    BuildVirtualTree();
+  }
+
+  void Write (std::ostream &os, bool binary) const;
+
+  ~ContextDependencyMulti() {
+    for (int i = 0; i < single_trees_.size(); i++) {
+      delete single_trees_[i];
+    }
+
+    delete to_pdf_;
+  }
+
+  const EventMap &ToPdfMap() const { return *to_pdf_; }
+
+  /// GetPdfInfo returns a vector indexed by pdf-id, saying for each pdf which
+  /// pairs of (phone, pdf-class) it can correspond to.  (Usually just one).
+  /// c.f. hmm/hmm-topology.h for meaning of pdf-class.
+
+  void GetPdfInfo(const std::vector<int32> &phones,  // list of phones
+                  const std::vector<int32> &num_pdf_classes,  // indexed by phone,
+                  std::vector<std::vector<std::pair<int32, int32> > > *pdf_info)
+      const;
+
+  void BuildVirtualTree() {
+
+  }
+
+ private:
+  int32 N_;  //
+  int32 P_;
+  EventMap *to_pdf_;  // owned here. This is the virtual tree
+  vector<EventMap*> single_trees_; // single trees, owned here
+
+  KALDI_DISALLOW_COPY_AND_ASSIGN(ContextDependencyMulti);
+};
+
+// Important note:
+// Statistics for training decision trees will be of type:
+// std::vector<std::pair<EventType, Clusterable*> >
+// We don't make this a typedef as it doesn't add clarity.
+// they will be sorted and unique on the EventType member, which
+// itself is sorted and unique on the name (see event-map.h).
+
+// See build-tree.h for functions relating to actually building the decision trees.
+
+
+
+
+}  // namespace Kaldi
+
+
+#endif
+
--- src/tree/context-dep.cc
+++ src/tree/context-dep.cc
@@ -43,27 +43,6 @@ bool ContextDependency::Compute(const std::vector<int32> &phoneseq,
   return to_pdf_->Map(event_vec, pdf_id);
 }
 
-bool ContextDependencyMulti::Compute(const std::vector<int32> &phoneseq,
-                                     int32 pdf_class,
-                                     int32 *pdf_id) const {
-  KALDI_ASSERT(static_cast<int32>(phoneseq.size()) == N_);
-  EventType event_vec;
-  event_vec.reserve(N_+1);
-  event_vec.push_back(std::make_pair
-                      (static_cast<EventKeyType>(kPdfClass),  // -1
-                       static_cast<EventValueType>(pdf_class)));
-  KALDI_COMPILE_TIME_ASSERT(kPdfClass < 0);  // or it would not be sorted.
-  for (int32 i = 0;i < N_;i++) {
-    event_vec.push_back(std::make_pair
-                        (static_cast<EventKeyType>(i),
-                         static_cast<EventValueType>(phoneseq[i])));
-    KALDI_ASSERT(static_cast<EventAnswerType>(phoneseq[i]) != -1);  // >=0 ?
-  }
-  KALDI_ASSERT(pdf_id != NULL);
-  return to_pdf_->Map(event_vec, pdf_id);
-}
-
-
 ContextDependency *GenRandContextDependency(const std::vector<int32> &phone_ids,
                                             bool ensure_all_covered,
                                             std::vector<int32> *hmm_lengths) {
@@ -169,47 +148,6 @@ void ContextDependency::Write (std::ostream &os, bool binary) const {
   WriteToken(os, binary, "EndContextDependency");
 }
 
-void ContextDependencyMulti::Write (std::ostream &os, bool binary) const {
-  WriteToken(os, binary, "ContextDependencyMulti");
-  WriteBasicType(os, binary, N_);
-  WriteBasicType(os, binary, P_);
-  WriteToken(os, binary, "Trees");
-  WriteBasicType(os, binary, single_trees_.size());
-  for (int i = 0; i < single_trees_.size(); i++) {
-    single_trees_[i]->Write(os, binary);
-  }
-  WriteToken(os, binary, "ToPdf");
-  to_pdf_->Write(os, binary);
-  WriteToken(os, binary, "EndContextDependencyMulti");
-}
-
-void ContextDependencyMulti::Read(std::istream &is, bool binary) {
-  if (to_pdf_) {
-    delete to_pdf_;
-    to_pdf_ = NULL;
-  }
-  for (int i = 0; i < single_trees_.size(); i++) {
-    if (single_trees_[i]) {
-      delete single_trees_[i];
-    }
-  }
-  ExpectToken(is, binary, "ContextDependencyMulti");
-  ReadBasicType(is, binary, &N_);
-  ReadBasicType(is, binary, &P_);
-  ExpectToken(is, binary, "Trees");
-  size_t size;
-  ReadBasicType(is, binary, &size);
-  single_trees_.resize(size);
-
-  for (int i = 0; i < size; i++) {
-    single_trees_[i] = EventMap::Read(is, binary);
-  }
-
-  ExpectToken(is, binary, "ToPdf");
-  to_pdf_ = EventMap::Read(is , binary);
-  ExpectToken(is, binary, "EndContextDependencyMulti");
-}
-
 void ContextDependency::Read (std::istream &is, bool binary) {
   if (to_pdf_) {
     delete to_pdf_;
@@ -275,51 +213,6 @@ void ContextDependency::GetPdfInfo(const std::vector<int32> &phones,
     KALDI_ASSERT(IsSortedAndUniq( ((*pdf_info)[i])));  // should have no dups.
   }
 }
-
-void ContextDependencyMulti::GetPdfInfo(
-         const std::vector<int32> &phones,
-         const std::vector<int32> &num_pdf_classes,  // indexed by phone,
-         std::vector<std::vector<std::pair<int32, int32> > > *pdf_info) const {  
-
-  EventType vec;
-  KALDI_ASSERT(pdf_info != NULL);
-  pdf_info->resize(NumPdfs());
-  for (size_t i = 0 ; i < phones.size(); i++) {
-    int32 phone = phones[i];
-    vec.clear();
-    vec.push_back(std::make_pair(static_cast<EventKeyType>(P_),
-                                 static_cast<EventValueType>(phone)));
-    // Now get length.
-    KALDI_ASSERT(static_cast<size_t>(phone) < num_pdf_classes.size());
-    EventAnswerType len = num_pdf_classes[phone];
-
-    for (int32 pos = 0; pos < len; pos++) {
-      vec.resize(2);
-      vec[0] = std::make_pair(static_cast<EventKeyType>(P_),
-                              static_cast<EventValueType>(phone));
-      vec[1] = std::make_pair(kPdfClass, static_cast<EventValueType>(pos));
-      std::sort(vec.begin(), vec.end());
-      std::vector<EventAnswerType> pdfs;  // pdfs that can be at this pos as this phone.
-      to_pdf_->MultiMap(vec, &pdfs);
-      SortAndUniq(&pdfs);
-      if (pdfs.empty()) {
-        KALDI_WARN << "ContextDependencyMulti::GetPdfInfo, "
-                   "no pdfs returned for position "
-                   << pos << " of phone " << phone
-                   << ".   Continuing but this is a serious error.";
-      }
-      for (size_t j = 0; j < pdfs.size(); j++) {
-        KALDI_ASSERT(static_cast<size_t>(pdfs[j]) < pdf_info->size());
-        (*pdf_info)[pdfs[j]].push_back(std::make_pair(phone, pos));
-      }
-    }
-  }
-  for (size_t i = 0; i < pdf_info->size(); i++) {
-    std::sort( ((*pdf_info)[i]).begin(),  ((*pdf_info)[i]).end());
-    KALDI_ASSERT(IsSortedAndUniq( ((*pdf_info)[i])));  // should have no dups.
-  }
-}
-
 
 ContextDependency*
 MonophoneContextDependency(const std::vector<int32> phones,
