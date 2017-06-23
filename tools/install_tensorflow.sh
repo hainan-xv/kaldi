9053aa1deed5659a3fd21c7aaeb945e54f55c190
Hainan Xu <hainanx@cltdell01gpu.clt.spoken.com>
2017-06-23 17:41:50 -0400

    small change in install_tensorflow script

--- tools/install_tensorflow.sh
+++ tools/install_tensorflow.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-export HOME=/export/b02/hxu
-export JAVA_HOME=/export/b02/hxu/TensorFlow/java/jdk1.8.0_121
-export PATH=/export/b02/hxu/TensorFlow/java/jdk1.8.0_121/bin/:$PATH
-export PATH=$PWD/bazel/output/:$PATH
+export HOME=$PWD/tensorflow_build/
+mkdir -p $HOME
 
 [ ! -f bazel.zip ] && wget https://github.com/bazelbuild/bazel/releases/download/0.5.1/bazel-0.5.1-dist.zip -O bazel.zip
 mkdir -p bazel
@@ -14,11 +12,11 @@ unzip ../bazel.zip
 ./compile.sh
 cd ../
 
-## now bazel is built
+# now bazel is built
 git clone https://github.com/tensorflow/tensorflow
 cd tensorflow
 ./configure
 
 tensorflow/contrib/makefile/download_dependencies.sh 
 bazel build --copt=-msse4.2 //tensorflow:libtensorflow.so
-#bazel build //tensorflow:libtensorflow_cc.so
+bazel build --copt=-msse4.2 //tensorflow:libtensorflow_cc.so
