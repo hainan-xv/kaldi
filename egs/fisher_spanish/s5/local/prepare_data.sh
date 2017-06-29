fc5fd91fd2b1ad79032e05d5385f593bbe264d0c
Hainan Xu <hainan.xv@gmail.com>
2017-06-28 20:04:28 -0400

    data/train/ finished

--- egs/fisher_spanish/s5/local/prepare_data.sh
+++ egs/fisher_spanish/s5/local/prepare_data.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-stage=3
+stage=1
 speechpath=$1
 transpath=$2
 
 tmpdir=data/local/texttmp
 dir=data/train
+
+export LC_ALL=C
+
 mkdir -p $tmpdir
 mkdir -p $dir
 
 if [ $stage -le 1 ]; then # text
-cat $transpath/fisher_spa_tr/data/transcripts/* | awk -F '\t' 'NF==13' | gzip > $tmpdir/all_text.gz
+cat $transpath/fisher_spa_tr/data/transcripts/* | grep -v "file;unicode" | awk -F '\t' 'NF==13' | gzip > $tmpdir/all_text.gz
 
-  zcat $tmpdir/all_text.gz | tail -n +2 | sed "s=_fsp.sph==g" | awk -F '\t' '{side="A";if($2==1){side="B"}printf("%s-%s_%06d_%06d\n",$1,side,int($3*100),int($4*100))}' > $tmpdir/utt-ids
-  zcat $tmpdir/all_text.gz | tail -n +2 | awk -F '\t' '{print $8}' > $tmpdir/rawtext.txt
+  zcat $tmpdir/all_text.gz | sed "s=_fsp.sph==g" | awk -F '\t' '{side="A";if($2==1){side="B"}printf("%s-%s_%06d_%06d\n",$1,side,int($3*100),int($4*100))}' > $tmpdir/utt-ids
+
+  cat $tmpdir/utt-ids | sort | uniq -c | sort -k1n | awk '$1!=1{print $2}' > $tmpdir/bad-utt-ids
+  cat $tmpdir/utt-ids | awk -F '_' '$(NF-1)==$NF{print $0}' >> $tmpdir/bad-utt-ids
+
+  zcat $tmpdir/all_text.gz | awk -F '\t' '{print $8}' > $tmpdir/rawtext.txt
 
   cat $tmpdir/rawtext.txt | \
       tr "A-Z" "a-z" | \
@@ -55,18 +62,19 @@ cat $transpath/fisher_spa_tr/data/transcripts/* | awk -F '\t' 'NF==13' | gzip > 
       sed "s=LAUGH=<laugh>=g" \
       > $tmpdir/cleantext.txt
     
-  paste $tmpdir/utt-ids $tmpdir/cleantext.txt > $dir/text
+  paste $tmpdir/utt-ids $tmpdir/cleantext.txt | sort | awk 'BEGIN{last=""}{if($1!=last) {print$0; last=$1};}' | grep -v -f $tmpdir/bad-utt-ids > $dir/text
 fi # generate data/train/text
 
 if [ $stage -le 2 ]; then # wav.scp
   find $speechpath/ -name "*.sph" > $tmpdir/sph.flist
   cat $tmpdir/sph.flist | awk -F '/' '{print $NF}' | sed "s=_fsp.sph==g" > $tmpdir/rec-ids
   paste $tmpdir/rec-ids $tmpdir/sph.flist | \
-    awk -v kaldi=$KALDI_ROOT '{printf("%s-A %s/tools/sph2pipe_v2.5/sph2pipe -f wav -p -c 1 %s |\n",$1,kaldi,$2); printf("%s-B %s/tools/sph2pipe_v2.5/sph2pipe -f wav -p -c 2 %s |\n",$1,kaldi,$2)}' > $dir/wav.scp
+    awk -v kaldi=$KALDI_ROOT '{printf("%s-A %s/tools/sph2pipe_v2.5/sph2pipe -f wav -p -c 1 %s |\n",$1,kaldi,$2); printf("%s-B %s/tools/sph2pipe_v2.5/sph2pipe -f wav -p -c 2 %s |\n",$1,kaldi,$2)}' | \
+    sort | awk 'BEGIN{last=""}{if($1!=last) {print$0; last=$1};}' > $dir/wav.scp
 fi  
 
 if [ $stage -le 3 ]; then # segment and reco2file_and_channel and utt2spk
-  cat $tmpdir/utt-ids | awk -F '_' '{print $0, $1"_"$2"_"$3, $4 / 100, $5 / 100}' > $dir/segments
-  cat $tmpdir/rec-ids | awk -F '-' '{print $0"-A", $1" A"; print $0"-B", $1" B"}' > $dir/reco2file_and_channel
-  cat $tmpdir/utt-ids | awk -F '_' '{print $0, $1"_"$2"_"$3}' > $dir/utt2spk
+  cat $tmpdir/utt-ids | awk -F '_' '{print $0, $1"_"$2"_"$3, $4 / 100, $5 / 100}' | sort | awk 'BEGIN{last=""}{if($1!=last) {print$0; last=$1};}' | grep -v -f $tmpdir/bad-utt-ids > $dir/segments
+  cat $tmpdir/rec-ids | awk -F '-' '{print $0"-A", $1" A"; print $0"-B", $1" B"}' | sort | grep -v -f $tmpdir/bad-utt-ids | awk 'BEGIN{last=""}{if($1!=last) {print$0; last=$1};}' > $dir/reco2file_and_channel
+  cat $tmpdir/utt-ids | awk -F '_' '{print $0, $1"_"$2"_"$3}' | sort -u | grep -v -f $tmpdir/bad-utt-ids > $dir/utt2spk
 fi
--- egs/fisher_spanish/s5/run.sh
+++ egs/fisher_spanish/s5/run.sh
@@ -3,8 +3,14 @@
 . cmd.sh
 . path.sh
 
+mfccdir=mfcc
+
 sfisher_speech=/export/a16/gkumar/corpora/LDC2010S01
 sfisher_transcripts=/export/a16/gkumar/corpora/LDC2010T04
 spanish_lexicon=/export/a16/gkumar/corpora/LDC96L16
 
 local/prepare_data.sh $sfisher_speech $sfisher_transcripts
+
+utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
+
+steps/make_mfcc.sh --nj 20 --cmd "$train_cmd" data/train exp/make_mfcc/train $mfccdir
